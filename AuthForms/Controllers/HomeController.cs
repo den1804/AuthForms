a7420b8980109a21bf92edcfb31419eee3e3d718
Підвашецький Денис <pod.den1804@gmail.com>
2017-05-31 21:17:00 +0300

    create

---
+++ AuthForms/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthForms.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            string result = "Вы не авторизованы";
+            if(User.Identity.IsAuthenticated)
+            {
+                result = "Ващ логин: " + User.Identity.Name;
+            }
+            return result;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
